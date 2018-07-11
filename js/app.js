12780a303d8a79b5bfc3220c9717dbdba9357fbd
Jayce <callrj5@gmail.com>
2018-07-11 14:44:54 -0600

    added global variables, added initial prompt, and listener for DOM content loaded

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@
 const addExpenseForm = document.querySelector('#add-expense');
 
 
+let budget, userBudget;
+
 
 
 
@@ -17,7 +19,14 @@ const addExpenseForm = document.querySelector('#add-expense');
 eventListeners();
 function eventListeners() {
 
+        // App Init
+        document.addEventListener('DOMContentLoaded', function() {
+           // Ask the weekly budget
+            userBudget = prompt(' What\'s your budget for this week? ');
+        });
 
+
+        // When new expense is added
         addExpenseForm.eventListeners('submit', function(e) {
             e.preventDefault();
         });
