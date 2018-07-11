d6398fb0758617ea42fc92fec907e1969553b695
Jayce <callrj5@gmail.com>
2018-07-11 16:37:37 -0600

    added budget gets recorded to budget left and budget total

--- js/app.js
+++ js/app.js
@@ -1,17 +1,34 @@
 // Classes
 
+class Budget {
+    constructor(budget) {
+        this.budget = Number ( budget );
+        this.budgetLeft = this.budget;
+    }
+}
 
+// Everything related to HTML
+class HTML {
 
+    //Insert budget when is submited
+    insertBudget(amount) {
 
-
+        budgetTotal.innerHTML = `${amount}`;
+        budgetLeft.innerHTML = `${amount}`;
+    }
+}
 
 // Variables
-const addExpenseForm = document.querySelector('#add-expense');
+const addExpenseForm = document.querySelector('#add-expense'),
+
+    budgetTotal = document.querySelector('span#total'),
+    budgetLeft = document.querySelector('span#left');
+
 
 
 let budget, userBudget;
 
-
+const html = new HTML();
 
 
 // Listeners
@@ -23,11 +40,21 @@ function eventListeners() {
         document.addEventListener('DOMContentLoaded', function() {
            // Ask the weekly budget
             userBudget = prompt(' What\'s your budget for this week? ');
+            // validate budget
+            if(userBudget === null || userBudget === '' || userBudget === '0') {
+                    window.location.reload();
+            } else {
+                // Budget is valid than instanciate the budget class
+                budget = new Budget(userBudget);
+
+                // Instantiate html class
+                html.insertBudget(budget.budget);
+            }
         });
 
 
         // When new expense is added
-        addExpenseForm.eventListeners('submit', function(e) {
+        addExpenseForm.addEventListeners('submit', function(e) {
             e.preventDefault();
         });
 }
